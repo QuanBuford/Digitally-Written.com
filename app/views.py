59faa5e248c0c6985f5b967ccff0b2af69de8e2e
Quan <jbuford@basecampcodingacademy.org>
2025-01-14 21:00:03 -0600

    push for everyone to get on same page

--- app/views.py
+++ app/views.py
@@ -51,11 +51,11 @@ def logout_PAGE(request):
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'post_detail.html', {'post': post})
 
 
 def create_post(request):
@@ -69,7 +69,7 @@ def create_post(request):
             return redirect('post_list')
     else:
         form = PostForm()
-    return render(request, 'blog/post_form.html', {'form': form})
+    return render(request, 'post_form.html', {'form': form})
 
 
 def edit_post(request, pk):
@@ -85,4 +85,4 @@ def edit_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_form.html', {'form': form})
+    return render(request, 'post_form.html', {'form': form})
