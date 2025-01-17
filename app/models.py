18238fb922ea9b7c2c816a5b5aa2546421e5cfd2
Brittney Bean <bbean@basecampcodingacademy.org>
2025-01-16 19:47:20 -0600

    bb updated bubbles for post no other changes made

--- app/models.py
+++ app/models.py
@@ -14,10 +14,10 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to User model
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post.title}"
+        return f"{self.author.username} - {self.text[:20]}"
